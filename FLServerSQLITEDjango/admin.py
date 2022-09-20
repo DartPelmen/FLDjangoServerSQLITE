6739e91fda81d24a16df02244a08aec013f8d1bc
Ivan Kuznetsov <kyzaiv1@yandex.ru>
2022-09-20 17:03:12 +1000

    Initial commit

---
+++ FLServerSQLITEDjango/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from FLServerSQLITEDjango.models import Table1
+
+# Register your models here.
+admin.site.register(Table1)
---
+++ FLServerSQLITEDjango/views.py
@@ -0,0 +1,30 @@
+import json
+
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+from FLServerSQLITEDjango import models
+from FLServerSQLITEDjango.models import Table1
+
+
+# Create your views here.
+def getAll(request):
+    output = []
+    out = models.Table1.objects.filter(id=1).values()
+    return JsonResponse({"data1": out[0]['data1']})
+
+
+@csrf_exempt
+def insertRow(request):
+    if request.method == 'POST':
+        row = json.loads(request.body)
+        if request.body is None:
+            return JsonResponse({"status": "INVALID INPUT"})
+        origin = models.Table1.objects.filter(id=1).values()[0]
+        origin['data2'] = row['data2']
+        table = Table1(id=1, data1=origin['data1'], data2=origin['data2'], data3=origin['data3'], data4=origin['data4'])
+        table.save()
+        return JsonResponse({"status": "OK"})
+    else:
+        return JsonResponse({"status": "NOT POST"})
